a563df63801ff6fdb2de5a719b08fc7c38d6ce18
houndhollis <houndhollis9797@gmail.com>
2025-04-16 21:31:03 +0900

    chore: 테스트코드 린트

--- src/basic/__tests__/basic.test.js
+++ src/basic/__tests__/basic.test.js
@@ -83,35 +83,35 @@ describe('basic test', () => {
     });
     
     
-      it('총액이 올바르게 계산되는지 확인', () => {
-        const mockDate=new Date('2025-04-16');
-        vi.useFakeTimers()
-        vi.setSystemTime(mockDate);
-        sel.value='p1';
+    it('총액이 올바르게 계산되는지 확인', () => {
+      const mockDate=new Date('2025-04-16');
+      vi.useFakeTimers()
+      vi.setSystemTime(mockDate);
+      sel.value='p1';
+      addBtn.click();
+      addBtn.click();
+      expect(sum.textContent).toContain('총액: 20000원(포인트: 20)');
+    });
+
+    it('할인이 올바르게 적용되는지 확인', () => {
+      const mockDate=new Date('2025-04-16');
+      vi.useFakeTimers()
+      vi.setSystemTime(mockDate);
+      sel.value='p1';
+      for (let i=0; i < 10; i++) {
         addBtn.click();
-        addBtn.click();
-        expect(sum.textContent).toContain('총액: 20000원(포인트: 20)');
-      });
-  
-      it('할인이 올바르게 적용되는지 확인', () => {
-        const mockDate=new Date('2025-04-16');
-        vi.useFakeTimers()
-        vi.setSystemTime(mockDate);
-        sel.value='p1';
-        for (let i=0; i < 10; i++) {
-          addBtn.click();
-        }
-        expect(sum.textContent).toContain('(10.0% 할인 적용)');
-      });
-  
-      it('포인트가 올바르게 계산되는지 확인', () => {
-        const mockDate=new Date('2025-04-16');
-        vi.useFakeTimers()
-        vi.setSystemTime(mockDate);
-        sel.value='p2';
-        addBtn.click();
-        expect(document.getElementById('loyalty-points').textContent).toContain('(포인트: 128)');
-      });
+      }
+      expect(sum.textContent).toContain('(10.0% 할인 적용)');
+    });
+
+    it('포인트가 올바르게 계산되는지 확인', () => {
+      const mockDate=new Date('2025-04-16');
+      vi.useFakeTimers()
+      vi.setSystemTime(mockDate);
+      sel.value='p2';
+      addBtn.click();
+      expect(document.getElementById('loyalty-points').textContent).toContain('(포인트: 128)');
+    });
     
 
     it('번개세일 기능이 정상적으로 동작하는지 확인', () => {
